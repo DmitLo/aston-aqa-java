0a75dd7908d4f268ba3a58726171376c435ade10
Dzmitry Los <d_los@bk.ru>
2023-11-27 23:55:42 +0300

    initial commit

--- lesson-12-testing/src/test/java/org/example/AppTest.java
+++ lesson-12-testing/src/test/java/org/example/AppTest.java
@@ -1,38 +1,34 @@
 package org.example;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.testng.annotations.Test;
+
+import static org.testng.AssertJUnit.assertEquals;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+public class AppTest {
+
+    @Test
+    public void testCalculateFactorialForZero(){
+        long result = App.calculateFactorial(0);
+        assertEquals(result, 1);
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
+    @Test
+    public void testCalculateFactorialForOne(){
+        long result = App.calculateFactorial(1);
+        assertEquals(result, 1);
     }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+    @Test
+    public void testCalculateFactorialForPositiveNumber(){
+        long result = App.calculateFactorial(5);
+        assertEquals(result, 120);
+    }
+
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void testCalculateFactorialForNegativeNumber(){
+        App.calculateFactorial(-5);
     }
 }
