fd8e622d57f5c35e97a6c759550b97ba4d6cbc25
Dzmitry Los <d_los@bk.ru>
2023-11-28 17:14:18 +0300

    initial commit

--- lesson-11-gradle/src/main/java/org/example/Main.java
+++ lesson-11-gradle/src/main/java/org/example/Main.java
@@ -1,5 +1,10 @@
 package org.example;
 
+/*
+Создайте проект, используя Gradle.
+В проект добавьте зависимости на такие инструменты как JUnit 5, Selenium.
+*/
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
