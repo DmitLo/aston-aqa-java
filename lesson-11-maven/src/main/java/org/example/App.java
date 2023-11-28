a467c66ba74b55e7e9566e4949c7457358a98cbc
Dzmitry Los <d_los@bk.ru>
2023-11-28 17:16:47 +0300

    initial commit

--- lesson-11-maven/src/main/java/org/example/App.java
+++ lesson-11-maven/src/main/java/org/example/App.java
@@ -1,13 +1,11 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+/*
+Создайте проект, используя Maven с архетипом quickstart.
+В проект добавьте зависимости на такие инструменты как JUnit 5, Selenium.
+*/
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
