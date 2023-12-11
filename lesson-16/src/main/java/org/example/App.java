904e07bc3f2943353ae292bc67473b984d76479b
Dzmitry Los <d_los@bk.ru>
2023-12-11 20:14:08 +0300

    initial commit

--- lesson-16/src/main/java/org/example/App.java
+++ lesson-16/src/main/java/org/example/App.java
@@ -1,9 +1,12 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
+/*
+ Практическое задание Л16
+ Перейдите в сервис Postman.
+ Войдите в свой аккаунт и скачайте себе коллекцию Postman Echo.
+ Написать автотесты для каждого метода из папки Request Methods
+ (проверка тела ответа (просто сравнить значения всех полей) и кода ответа).
+*/
 public class App 
 {
     public static void main( String[] args )
--- lesson-16/src/test/java/org/example/ApiTest.java
+++ lesson-16/src/test/java/org/example/ApiTest.java
@@ -1,42 +1,125 @@
 package org.example;
 
 import io.restassured.RestAssured;
-import org.apache.http.HttpStatus;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-import static io.restassured.RestAssured.given;
-import static io.restassured.RestAssured.request;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.core.IsEqual.equalTo;
 
 public class ApiTest {
 
-    @Test(priority = 1)
-    public void testGetRequest(){
-
+    @BeforeTest
+    public static void setup() {
         RestAssured.baseURI = "https://postman-echo.com";
+    }
+
+
+    @Test(priority = 1)
+    public void testGetRequest() {
+
         given()
                 .when()
-                .get()
+                .get("/get")
                 .then()
                 .statusCode(200);
     }
 
     @Test(priority = 2)
-    public void testGetRequestWithDataValidation(){
+    public void testGetRequestWithDataValidation() {
 
-        RestAssured.baseURI = "https://postman-echo.com";
         given()
                 .when()
-                .get()
+                .get("/get?foo1=bar1&foo2=bar2")
                 .then()
-                .log()
-                .body()
                 .statusCode(200)
                 .and()
-                .body("foo1" )
-
+                .body("args.foo1", equalTo("bar1"))
+                .body("args.foo2", equalTo("bar2"));
     }
 
+    @Test(priority = 3)
+    public void testPostRequestWithDataValidation() {
 
+        // Определяем тело запроса (JSON, XML и т.д.)
+        String requestBody = "{\"test\": \"value\"}";
+        // Выполняем POST-запрос и сохраняем ответ
+        Response response = RestAssured.given()
+                .contentType(ContentType.JSON) // Устанавливаем тип содержимого тела запроса
+                .body(requestBody) // Устанавливаем тело запроса
+                .post("/post"); // Указываем конечную точку (endpoint)
+        // Выводим детали ответа
+        Assert.assertEquals(200, response.statusCode());
+        Assert.assertEquals("value", response.getBody().jsonPath().getString("data.test"));
+    }
 
+    @Test(priority = 4) //
+    public void testPostRequest() {
 
+        String requestBody = "{\"foo1\": \"bar1\", \"foo2\": \"bar2\"}";
+
+        Response response = given()
+                .header("Content-type", "application/json")
+                .and()
+                .body(requestBody)
+                .when()
+                .post("/post")
+                .then()
+                .extract().response();
+
+        Assert.assertEquals(200, response.statusCode());
+    }
+
+    @Test(priority = 5)
+    public void testPutRequest() {
+
+        String requestBody = "{\"data\" : \"This is expected to be sent back as part of response body.\"}";
+
+        Response response = given()
+                .header("Content-type", "application/json")
+                .and()
+                .body(requestBody)
+                .when()
+                .put("/put")
+                .then()
+                .extract().response();
+
+        Assert.assertEquals(200, response.statusCode());
+        Assert.assertEquals("[data:This is expected to be sent back as part of response body.]", response.jsonPath().getString("data"));
+    }
+
+    @Test(priority = 6)
+    public void testPatchRequest() {
+
+        String requestBody = "{\"data\" : \"This is expected to be sent back as part of response body.\"}";
+
+        Response response = given()
+                .header("Content-type", "application/json")
+                .and()
+                .body(requestBody)
+                .when()
+                .patch("/patch")
+                .then()
+                .extract().response();
+
+        Assert.assertEquals(200, response.statusCode());
+        Assert.assertEquals("[data:This is expected to be sent back as part of response body.]", response.jsonPath().getString("data"));
+    }
+
+    @Test(priority = 7)
+    public void testDeleteRequest() {
+
+        RequestSpecification httpRequest = RestAssured.given().header("Authorization", "postman-token "
+                + "e6aacf6a-4dd2-4f2b-946d-c1df740f0f22").header("Content-Type", "application/json");
+
+        // Calling the Delete API with request body
+        Response res = httpRequest.body("{ \"data\": \"This is expected to be sent back as part of response body.\"}").delete("/delete");
+        // Fetching the response code from the request and validating the same
+        Assert.assertEquals(res.getStatusCode(),200);
+    }
 }
+
